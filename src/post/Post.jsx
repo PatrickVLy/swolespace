b8b694097c569846ec528c6db6eb09c22c19fb43
Patrick Ly <patrickly72@gmail.com>
2022-01-21 13:36:31 -0800

    created post component and style

---
+++ src/post/Post.jsx
@@ -0,0 +1,19 @@
+import "./post.css"
+
+export default function Post() {
+    return (
+        <div className="post">
+            <img className="postImg" src="./squat.jpg"/>
+            <div className="postInfo">
+                <div className="postCategories">
+                    <span className="postCategory">Legs</span>
+                </div>
+                <span className="postTitle">Insane Legday</span>
+                <hr/>
+                <span className="postDate">1 hour ago</span>
+                <p className="postDescription">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto, assumenda eum minus repellat perferendis sapiente nemo laborum dicta blanditiis. Dolore sint quis fuga cupiditate incidunt harum enim. Repellendus, doloremque? Culpa.</p>
+            </div>
+
+        </div>
+    )
+}
