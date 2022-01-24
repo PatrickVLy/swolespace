a79ee439c2fc0829cffa95e923fa9ec2174ce4d9
Patrick Ly <patrickly72@gmail.com>
2022-01-23 16:05:24 -0800

    added single post component,typography selection for components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Home from "./pages/home/Home"
 import TopBar from "./components/topbar/TopBar"
+import Single from "./pages/single/Single"
+import Posts from "./components/posts/Posts";
 
 function App() {
   return (
     <>
       <TopBar/>
-      <Home/>
+      <Single/>
 
 
       
