6251a15017c4bd3048a56352b6341827e80602c1
Patrick Ly <patrickly72@gmail.com>
2022-01-25 23:27:28 -0800

    created backend routes for signup, login, update user, and delete user

--- api/index.js
+++ api/index.js
@@ -2,14 +2,21 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
-const authRoute = require("./routes/auth")
+const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
 
 dotenv.config();
-mongoose.connect(process.env.MONGO_URL).then(console.log("connected to mongoDB"))
+app.use(express.json());
+
+mongoose
+.connect(process.env.MONGO_URL)
+.then(console.log("connected to mongoDB"))
 .catch((err)=>console.log(err));
 
 
-app.use("/api/auth", authRoute)
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+
 
 app.listen("5000", ()=> {
     console.log("now listening on port 5000");
--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,19 +1,38 @@
-const router = require("express").Router()
-const User = require("../models/User")
+const router = require("express").Router();
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
 
 
 //SIGNUP
 router.post("/signup", async (req,res)=>{
     try {
-        constnewUser = new User({
+        const salt = await bcrypt.genSalt(10);
+        const hashedPass = await bcrypt.hash(req.body.password, salt);
+        const newUser = new User({
             email: req.body.email, 
             username: req.body.username,
-            password: req.body.password,
+            password: hashedPass,
         });
         const user = await newUser.save();
         res.status(200).json(user)
-    } catch (error) {
+    } catch (err) {
         res.status(500).json(err);
+    }
+});
+
+//LOGIN
+router.post("/login", async (req, res)=>{
+    try {
+        const user= await User.findOne({ username: req.body.username });
+        !user && res.status(400).json("Invalid Username or Password");
+
+        const validated = await bcrypt.compare(req.body.password, user.password);
+        !validated && res.status(400).json("Invalid Username or Password");
+
+        const{password, ...others } = user._doc;
+        res.status(200).json(others);
+    } catch (error) {
+        res.status(500).json(error)
     }
 });
 
