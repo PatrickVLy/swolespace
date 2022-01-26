62cda06a17f47451e18fb13277bc9f642a5ffd10
Patrick Ly <patrickly72@gmail.com>
2022-01-26 15:26:44 -0800

    created post routes for posts to log workout, connected to DB

--- api/models/Post.js
+++ api/models/Post.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
-const PostSchema = new mongoose.Schema({
+const PostSchema = new mongoose.Schema(
+    {
     title:{
         type: String,
         required: true,
@@ -8,20 +9,130 @@ const PostSchema = new mongoose.Schema({
     }, 
     
     exercises:[
-        firstExercise, {
+    {
+        firstExercise: {
         type: String,
         required: true,
-    }, 
+        }, 
 
-    firstExerciseWeight, {
+        firstExerciseWeight: {
         type: Number,
         required: true,
-    }, 
+        }, 
     
-    firstExerciseWeight, {
+        firstExerciseSets: {
         type: Number,
         required: true,
-    }, ],
+        },
+    
+        firstExerciseReps: {
+        type: Number,
+        required: true,
+        }, 
+    }, 
+    {
+        secondExercise: {
+        type: String,
+        required: false,
+        }, 
+
+        secondExerciseWeight: {
+        type: Number,
+        required: false,
+        }, 
+    
+        secondExerciseSets: {
+        type: Number,
+        required: false,
+        },
+    
+        secondExerciseReps: {
+        type: Number,
+        required: false,
+        }, 
+    }, 
+    {
+        thirdExercise: {
+        type: String,
+        required: false,
+        }, 
+
+        thirdExerciseWeight: {
+        type: Number,
+        required: false,
+        }, 
+    
+        thirdExerciseSets: {
+        type: Number,
+        required: false,
+        },
+    
+        thirdExerciseReps: {
+        type: Number,
+        required: false,
+        }, 
+    }, {
+        fourthExercise: {
+        type: String,
+        required: false,
+        }, 
+
+        fourthExerciseWeight: {
+        type: Number,
+        required: false,
+        }, 
+    
+        fourthExerciseSets: {
+        type: Number,
+        required: false,
+        },
+    
+        fourthExerciseReps: {
+        type: Number,
+        required: false,
+        }, 
+    }, {
+        fifthExercise: {
+        type: String,
+        required: false,
+        }, 
+
+        fifthExerciseWeight: {
+        type: Number,
+        required: false,
+        }, 
+    
+        fifthExerciseSets: {
+        type: Number,
+        required: false,
+        },
+    
+        fifthExerciseReps: {
+        type: Number,
+        required: false,
+        }, 
+    }, {
+        sixthExercise: {
+        type: String,
+        required: false,
+        }, 
+
+        sixthExerciseWeight: {
+        type: Number,
+        required: false,
+        }, 
+    
+        sixthExerciseSets: {
+        type: Number,
+        required: false,
+        },
+    
+        sixthExerciseReps: {
+        type: Number,
+        required: false,
+        }, 
+    }, 
+],
     
 
     description:{
