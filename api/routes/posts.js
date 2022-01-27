1f50a517fbdb9179b30b277ad85e1e3a2d48bc07
Patrick Ly <patrickly72@gmail.com>
2022-01-26 16:41:03 -0800

    added delete route for posts

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -39,23 +39,23 @@ try {
 });
 //DELETE
 router.delete("/:id", async (req,res)=>{
-    if (req.body.userId === req.params.id){
         try {
-            const user = await User.findById(req.params.id);
+            const post = await Post.findById(req.params.id);
+            if(post.username === req.body.username){
             try {
-                await Post.deleteMany({ username: user.username });
-                await User.findByIdAndDelete(req.params.id);
-                res.status(200).json("User has been deleted");
+                await post.delete();
+                res.status(200).json("post has been deleted");
             } catch (err) {
                 res.status(500).json(err);
             }
-        } catch (err) {
-            res.status(404).json("User not found")
+        } else {
+            res.status(401).json("Invalid Account")
         }
-    } else{
-        res.status(401).json("Incorrect Account")
     }
-});
+        catch (err) {
+            res.status(500).json("err")
+        }
+    });
 
 //GETUSER
 router.get("/:id", async (req,res)=>{
