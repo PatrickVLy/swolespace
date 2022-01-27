39dbb7427d6380089ae7ed3e56ee8e2ae99cc27f
Patrick Ly <patrickly72@gmail.com>
2022-01-26 16:22:26 -0800

    updated model for posts

--- api/models/Post.js
+++ api/models/Post.js
@@ -1,162 +1,46 @@
 const mongoose = require("mongoose");
 
 const PostSchema = new mongoose.Schema(
-    {
-    title:{
-        type: String,
-        required: true,
-        unique: true,
-    }, 
-    
-    exercises:[
-    {
-        firstExercise: {
-        type: String,
-        required: true,
-        }, 
+    { 
+    title:{ type: String, required: true, unique: false }, 
 
-        firstExerciseWeight: {
-        type: Number,
-        required: true,
-        }, 
-    
-        firstExerciseSets: {
-        type: Number,
-        required: true,
-        },
-    
-        firstExerciseReps: {
-        type: Number,
-        required: true,
-        }, 
-    }, 
-    {
-        secondExercise: {
-        type: String,
-        required: false,
-        }, 
+    firstExercise: { type: String, required: false }, 
+    firstExerciseWeight: { type: Number, required: false }, 
+    firstExerciseSets: { type: Number, required: false },
+    firstExerciseReps: { type: Number, required: false }, 
 
-        secondExerciseWeight: {
-        type: Number,
-        required: false,
-        }, 
-    
-        secondExerciseSets: {
-        type: Number,
-        required: false,
-        },
-    
-        secondExerciseReps: {
-        type: Number,
-        required: false,
-        }, 
-    }, 
-    {
-        thirdExercise: {
-        type: String,
-        required: false,
-        }, 
+    secondExercise: { type: String, required: false }, 
+    secondExerciseWeight: { type: Number, required: false }, 
+    secondExerciseSets: { type: Number, required: false },
+    secondExerciseReps: { type: Number, required: false }, 
 
-        thirdExerciseWeight: {
-        type: Number,
-        required: false,
-        }, 
-    
-        thirdExerciseSets: {
-        type: Number,
-        required: false,
-        },
-    
-        thirdExerciseReps: {
-        type: Number,
-        required: false,
-        }, 
-    }, {
-        fourthExercise: {
-        type: String,
-        required: false,
-        }, 
+    thirdExercise: { type: String, required: false }, 
+    thirdExerciseWeight: { type: Number, required: false }, 
+    thirdExerciseSets: { type: Number, required: false },
+    thirdExerciseReps: { type: Number, required: false }, 
 
-        fourthExerciseWeight: {
-        type: Number,
-        required: false,
-        }, 
-    
-        fourthExerciseSets: {
-        type: Number,
-        required: false,
-        },
-    
-        fourthExerciseReps: {
-        type: Number,
-        required: false,
-        }, 
-    }, {
-        fifthExercise: {
-        type: String,
-        required: false,
-        }, 
+    fourthExercise: { type: String, required: false }, 
+    fourthExerciseWeight: { type: Number,required: false }, 
+    fourthExerciseSets: { type: Number, required: false },
+    fourthExerciseReps: { type: Number, required: false }, 
 
-        fifthExerciseWeight: {
-        type: Number,
-        required: false,
-        }, 
-    
-        fifthExerciseSets: {
-        type: Number,
-        required: false,
-        },
-    
-        fifthExerciseReps: {
-        type: Number,
-        required: false,
-        }, 
-    }, {
-        sixthExercise: {
-        type: String,
-        required: false,
-        }, 
+    fifthExercise: { type: String, required: false }, 
+    fifthExerciseWeight: { type: Number, required: false }, 
+    fifthExerciseSets: { type: Number, required: false },
+    fifthExerciseReps: { type: Number, required: false }, 
 
-        sixthExerciseWeight: {
-        type: Number,
-        required: false,
-        }, 
-    
-        sixthExerciseSets: {
-        type: Number,
-        required: false,
-        },
-    
-        sixthExerciseReps: {
-        type: Number,
-        required: false,
-        }, 
-    }, 
-],
-    
+    sixthExercise: { type: String, required: false }, 
+    sixthExerciseWeight: { type: Number, required: false }, 
+    sixthExerciseSets: { type: Number, required: false },
+    sixthExerciseReps: {type: Number, required: false },  
 
-    description:{
-        type: String, 
-        required: true,
-        unique: true,
-    }, 
+    description: { type: String, required: true, unique: false}, 
     
-    
-    photo:{
-        type: String, 
-        required: false,
-        default: "",
-    }, 
+    photo:{ type: String, required: false, default: ""}, 
 
-    username:{
-        type: String,
-        required: true,
-    }, 
+    username:{ type: String, required: true}, 
 
-    categories:{
-        type: Array,
-        required: false,
-    }, 
+    categories:{ type: Array, required: false}, 
 },
 { timestamps: true }
 );
